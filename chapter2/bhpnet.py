e1a931219ef0400eef39c42ed006eb1e7f02d47e
wangjin0818 <wangjin0818@gmail.com>
2015-01-13 10:14:22 +0800

    Create the project

---
+++ chapter2/bhpnet.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+# define some global variables
+listen                  = False
+command                 = False
